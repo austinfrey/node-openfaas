83f56e713126aa20ff157edb78be1ac6c2282e00
Austin Frey <aafrey85@gmail.com>
2017-09-24 14:21:19 -0400

    Added Nock for API mocking
    Signed-off-by: Austin Frey <aafrey85@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const Faas = require('./faas');
+'use strict';
 
-const faas = Faas('http://localhost:8080');
-
-faas.deploy(
-	'yolo',
-	'func_functions',
-	'hello-serverless'
-)
-	.then(x => console.log(x))
-	.catch(err => console.log(err));
+module.exports = require('./faas');
 
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const test = require('tape');
-const BbPromise = require('bluebird')
+const nock = require('nock');
+const BbPromise = require('bluebird');
 const FaaS = require('./faas');
 
 test('Test typeofs', t => {
@@ -12,21 +13,28 @@ test('Test typeofs', t => {
 	const faas = FaaS('http://localhost:8080');
 
 	t.equals(typeof faas, 'object');
-	t.equals(typeof faas.deploy, 'function')
-	t.equals(typeof faas.invoke, 'function')
-	t.equals(typeof faas.compose, 'function')
-	t.equals(typeof faas.remove, 'function')
-})
+	t.equals(typeof faas.deploy, 'function');
+	t.equals(typeof faas.invoke, 'function');
+	t.equals(typeof faas.compose, 'function');
+	t.equals(typeof faas.remove, 'function');
+});
 
 test('Test full API', t => {
-	t.plan(4)
-	const faas = FaaS('http://localhost:8080')
 
-	const delay = (time) => {
-		return new BbPromise(resolve => {
-			setTimeout(resolve, time)
-		})
-	}
+	nock('http://localhost:8080')
+		.post('/system/functions', {
+			service: 'test-func',
+			network: 'func_functions',
+			image: 'hello-serverless'
+		}).reply(200)
+		.post('/function/test-func').reply(200, {status: 'done'})
+		.post('/function/func_nodeinfo').reply(200, 'hello cruel world')
+		.post('/function/func_echoit', 'hello cruel world').reply(200, 'hello cruel world')
+		.post('/function/func_wordcount', 'hello cruel world').reply(200, 3)
+		.delete('/system/functions', {functionName: 'test-func'}).reply(200);
+
+	t.plan(5);
+	const faas = FaaS('http://localhost:8080');
 
 	faas.deploy(
 		'test-func',
@@ -34,15 +42,18 @@ test('Test full API', t => {
 		'hello-serverless'
 	)
 		.then(x => t.equals(x.statusCode, 200))
-		.then(() => delay(5000).then(() => faas.invoke('test-func')))
+		.then(() => faas.invoke('test-func'))
 		.then(x => t.same(x.body, {status: 'done'}))
 		.then(() => faas.compose('', [
-				'func_nodeinfo',
-				'func_echoit',
-				'func_wordcount'
-			]
+			'func_nodeinfo',
+			'func_echoit',
+			'func_wordcount'
+		]
 		))
-		.then(x => t.equals(x.statusCode, 200))
+		.then(x => {
+			t.equals(x.statusCode, 200)
+			t.equals(x.body, '3')
+		})
 		.then(() => faas.remove('test-func'))
 		.then(x => t.equals(x.statusCode, 200))
 		.catch(err => console.log(err));
