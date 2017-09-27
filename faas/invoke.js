9100223acd11c92c767773b57fb2eaf7aed17f46
ericstoekl <ems5311@gmail.com>
2017-09-27 05:30:33 +0000

    Allow encoding: option in invoke command

--- faas/invoke.js
+++ faas/invoke.js
@@ -6,11 +6,12 @@ const got = require('got');
 const invoke = gateway => {
 	const url = gateway;
 
-	return (name, data = null, isJson = false) => {
+	return (name, data = null, isJson = false, isBinaryResponse = false) => {
 		const funcPath = path.join('/function', name);
 		const options = {
 			method: 'POST',
-			json: isJson
+			json: isJson,
+                        encoding: (isBinaryResponse ? null : 'utf8')
 		};
 
 		if (data) {
